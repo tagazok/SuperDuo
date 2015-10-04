d83eb89de1d2cf52d179e5ef302593d032642a42
oleplus <oleplus@oleplus-macbookair.roam.corp.google.com>
2015-10-04 16:37:29 +0200

    first commit

---
+++ alexandria/app/src/main/java/it/jaschke/alexandria/ScannerActivity.java
@@ -0,0 +1,41 @@
+package it.jaschke.alexandria;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+import com.google.zxing.Result;
+
+public class ScannerActivity extends Activity implements ZXingScannerView.ResultHandler {
+    private ZXingScannerView mScannerView;
+
+    @Override
+    public void onCreate(Bundle state) {
+        super.onCreate(state);
+        mScannerView = new ZXingScannerView(this);   // Programmatically initialize the scanner view
+        setContentView(mScannerView);                // Set the scanner view as the content view
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.
+        mScannerView.startCamera();          // Start camera on resume
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mScannerView.stopCamera();           // Stop camera on pause
+    }
+
+    @Override
+    public void handleResult(Result rawResult) {
+        // Do something with the result here
+        Log.v("scanresult", rawResult.getText()); // Prints scan results
+        Log.v("qrcode", rawResult.getBarcodeFormat().toString()); // Prints the scan format (qrcode, pdf417 etc.)
+    }
+}
